1ebd5b215e6db20db1db2c3240895b29a9eaace3
Lars Pettersson <lars.pettersson@biol.lu.se>
2018-10-11 15:11:55 +0200

    added automate_run_2.bash, script in bash

---
+++ automate_run_2.bash
@@ -0,0 +1,23 @@
+#################################
+# Adjust your path on your machine
+#output directory where we will save results
+RESULT_DIR=/Users/lars/Desktop/sw-carpentry/results   #$PWD/results
+#input directory for molecules data
+INPUT_DIR=/Users/lars/Desktop/sw-carpentry/molecules
+#Do not change here except you know what you are doing!
+
+rm -rf $RESULT_DIR
+mkdir $RESULT_DIR
+touch $RESULT_DIR/length.txt
+for filename in $INPUT_DIR/*.pdb
+
+    do
+
+   wc -l $filename | sort -n | head -n 1 >> $RESULT_DIR/lengths.txt
+
+done
+
+cat $RESULT_DIR/lengths.txt
+
+
+####################################
